82286890331baa9260f019ab4e5d00dd135c497c
thinkphp9747 <yanyh9747@163.com>
2017-02-19 20:34:58 +0800

    1、使用横版继承。 2、增加员工模型后台（还未在系统中使用）

---
+++ application/index/model/yg.php
@@ -0,0 +1,92 @@
+<?php
+namespace app\index\model;
+use think\Model;    // 调用模型类
+/**
+ * 员工模型类
+ */
+class yg extends Model
+{
+    /**
+     * 用户登录
+     * @param  string $username 用户名
+     * @param  string $password 密码
+     * @return bool   成功返回true，失败返回false。
+     */
+    static public function login($username, $password)
+    {
+        // 验证用户是否存在
+        $map = array('username' => $username);
+        $yg = self::get($map);
+    
+        if (!is_null($yg)) {
+            // 验证密码是否正确
+            if ($yg->checkPassword($password)) {
+                // 登录
+                session('ygId', $yg->getData('id'));
+                session('name',$yg->getData('username'));
+				session('jgbh',$yg->getData('机构号'));
+                
+                return true;
+            }
+        }
+    
+        return false;
+    }
+    
+    /**
+     * 验证密码是否正确
+     * @param  string $password 密码
+     * @return bool
+     */
+    public function checkPassword($password)
+    {
+        if ($this->getData('password') === $this::encryptPassword($password))
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    /**
+     * 密码加密算法
+     * @param    string                   $password 加密前密码
+     * @return   string                             		加密后密码
+     * @author panjie@yunzhiclub.com http://www.mengyunzhi.com
+     * @DateTime 2016-10-21T09:26:18+0800
+     */
+    static public function encryptPassword($password)
+    {
+        // ʵ实际的过程中，我还还可以借助其它字符串算法，来实现不同的加密。
+        return sha1(md5($password) . 'mengyunzhi');
+    }
+    
+    /**
+     * 注销
+     * @return bool 成功true，失败false。
+     * @author panjie
+     */
+    static public function logOut()
+    {
+        // 销毁session中数据
+        session('ygId', null);
+        return true;
+    }
+    
+    /**
+     * 判断用户是否已登录
+     * @return boolean 已登录true
+     * @author  panjie <panjie@yunzhiclub.com>
+     */
+    static public function isLogin()
+    {
+        $ygId = session('ygId');
+    
+        // isset()和is_null()是一对反义词
+        if (isset($ygId)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
