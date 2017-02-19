571bb290f86abc31fff814d49396f922ba0e5144
thinkphp9747 <yanyh9747@163.com>
2017-02-19 20:31:53 +0800

    1、使用模版继承重新设计页面。 2、引入员工模型，实现员工登录、退出等后台功能（还未在系统中使用）

--- application/index/controller/Home.php
+++ application/index/controller/Home.php
@@ -2,21 +2,30 @@
 namespace app\index\controller;
 use think\Controller;
 use think\Db;
-
+/**
+ * 缴费平台控制器
+ */
 class Home extends Controller
 {
+	/**
+	 * 缴费平台主页
+	 */
     public function index()
     {
+    	 $this->assign('ygid',10);
     	return view();    
 	}
-	
+	/**
+	 * 通报数据图表显示
+	 */
 	public function chart()
 	{
+		$this->assign('ygid',1);
 		return view();
 	}
 	
 	 /**
-     * AJAX返回检索数据
+     * AJAX返回检索数据填充图表
      * @return  $responce       返回json数据
      */
     public function json()
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,45 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
+use think\Request;              // 引用Request类库
+use app\index\model\yg;   // 引用yg模型类
+
+/**
+ * 系统登录控制器
+ */
+
 class Index extends Controller
 {
+     // 用户登录表单
     public function index()
     {
-    	return view();    
-	}
+        // 显示登录表单
+        return view();
+        
+    }
+    
+    // 处理用户提交的登录数据
+    public function login()
+    {
+        // 接收post信息
+        $postData = Request::instance()->post();
+        
+        // 直接调用M层方法，进行登录。
+        if (yg::login($postData['username'], $postData['password'])) {
+            return $this->success('login success', url('index/index'));
+        } else {
+            return $this->error('username or password incorrent', url('index'));
+        }
+        
+    }
+    
+    // 注销
+    public function logOut()
+    {
+        if (yg::logOut()) {
+            return $this->success('logout success', url('index'));
+        } else {
+            return $this->error('logout error', url('index/index'));
+        }
+    }
 }
