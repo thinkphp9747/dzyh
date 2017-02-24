d0c74d0b0db0470fc5d421d862b665e7b770f7e7
thinkphp9747 <yanyh9747@163.com>
2017-02-24 20:44:39 +0800

    引入PHPExcel 增加上传文件功能， 待增加Excel文件导入数据库

--- application/common.php
+++ application/common.php
@@ -10,3 +10,26 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+/**
+ * TODO 上传文件方法
+ * @param $fileid form表单file的name值
+ * @param $dir 上传到uploads目录的$dir文件夹里
+ * @param int $maxsize 最大上传限制，默认1024000 byte
+ * @param array $exts 允许上传文件类型 默认array('gif','jpg','jpeg','bmp','png')
+ * @return array 返回array,失败status=0 成功status=1,filepath=newspost/2014-9-9/a.jpg
+ */
+function uploadfile($fileid,$dir,$maxsize=5242880,$exts=array('xls','xlsx'),$maxwidth=430){
+    $upload = new \Think\Upload();// 实例化上传类
+    $upload->maxSize   =     $maxsize;// 设置附件上传大小，单位字节(微信图片限制1M
+    $upload->exts      =     $exts;// 设置附件上传类型
+    $upload->rootPath  =     '/public/uploads/'; // 设置附件上传根目录
+    $upload->savePath  =     $dir.'/'; // 设置附件上传（子）目录
+    // 上传文件
+    $info   =   $upload->upload();
+
+    if(!$info) {// 上传错误提示错误信息
+        return array(status=>0,msg=>$upload->getError());
+    }else{// 上传成功
+        return array(status=>1,msg=>'上传成功',filepath=>$info[$fileid]['savepath'].$info[$fileid]['savename']);
+    }
+}
--- application/index/controller/Home.php
+++ application/index/controller/Home.php
@@ -24,6 +24,71 @@ class Home extends Controller
 		return view();
 	}
 	
+	/**
+	 * 缴费平台数据导入上传
+	 */
+	 public function upjfsj()
+	 {
+	 	$this->assign('ygid',1);
+		return view();
+	 }
+	 
+	 /**
+	  * 缴费平台数据导入
+	  */
+	public function putjfsj()
+	{
+		 $file = request()->file('jfsj');
+	    // 移动到框架应用根目录/public/uploads/ 目录下
+	    $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+	    if($info){
+	        // 成功上传后 获取上传信息
+	        // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+	        $filepath = "/public/uploads/".$info->getSaveName();
+			
+			//引入PHPExcel类库
+			require_once '/public/PHPExcel/IOFactory.php';
+			
+			//获取excel文件
+			$objPHPExcel = \PHPExcel_IOFactory::load("$filepath");
+			$objPHPExcel->setActiveSheetIndex(0);
+			$sheet0=$objPHPExcel->getSheet(0);	
+			
+			//获取行数，并把数据读取出来$data数组
+			$rowCount=$sheet0->getHighestRow();//excel行数
+	        $data=array();
+	        for ($i = 2; $i <= $rowCount; $i++){
+	            $item['name']=$this->getExcelValue($sheet0,'A'.$i);
+	            $item['sex']=$this->getExcelValue($sheet0,'B'.$i);
+	            $item['contact']=$this->getExcelValue($sheet0,'C'.$i);
+	            $item['remark']=$this->getExcelValue($sheet0,'D'.$i);
+	            $item['addtime']=$this->getExcelValue($sheet0,'E'.$i);
+	
+	            $data[]=$item;
+	        }	
+			
+			//保存到数据
+			$success=0;
+			$error=0;
+			$sum=count($data);
+			foreach($data as $k=>$v){
+			    if(M('temp_area3')->data($v)->add()){
+			        $success++;
+			    }else {
+			        $error++;
+			        }
+			    }
+			
+			        echo "总{$sum}条，成功{$success}条，失败{$error}条。";
+			
+	        
+	    }else{
+	        // 上传失败获取错误信息
+	        echo $file->getError();
+	    }
+		
+	}
+	
 	 /**
      * AJAX返回检索数据填充图表
      * @return  $responce       返回json数据
