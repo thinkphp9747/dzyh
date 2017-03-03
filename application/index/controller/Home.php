813669e5a7a9c14ccd1f2505929bf9111e667f15
thinkphp9747 <yanyh9747@163.com>
2017-03-03 22:57:36 +0800

    增加PHPWord类库 增加模板标签写入并下载程序块

--- application/index/controller/Home.php
+++ application/index/controller/Home.php
@@ -201,4 +201,41 @@ class Home extends Controller
         echo json_encode($responce); // 转化为json数据格式
         
     }
+
+/**
+ * 调试填写模板文件并下载
+ */
+ public function downloadfile()
+ {
+ 	include_once '/PHPWord.php';
+	$PHPWord = new \PHPWord();
+	$document = $PHPWord->loadTemplate("D:/wamp/www/public/uploads/PHP.docx");
+	
+	$document->setValue('Value1', 'Sun');
+	$document->setValue('Value2', 'Mercury');
+	$document->setValue('Value3', 'Venus');
+	$document->setValue('Value4', 'Earth');
+	$document->setValue('Value5', 'Mars');
+	$document->setValue('Value6', 'Jupiter');
+	$document->setValue('Value7', 'Saturn');
+	$document->setValue('Value8', 'Uranus');
+	$document->setValue('Value9', 'Neptun');
+	$document->setValue('Value10', 'Pluto');
+	
+	$document->setValue('weekday', date('l'));
+	$document->setValue('time', date('H:i'));
+	
+	$document->save('D:/wamp/www/public/uploads/test.docx');
+ 	$filename=realpath("D:/wamp/www/public/uploads/test.docx"); //文件名
+	$date=date("Ymd-H:i:m");
+	Header( "Content-type:  application/octet-stream "); 
+	Header( "Accept-Ranges:  bytes "); 
+	Header( "Accept-Length: " .filesize($filename));
+	header( "Content-Disposition:  attachment;  filename= {$date}.docx"); 
+	echo file_get_contents($filename);
+	readfile($filename); 
+	$this->success("下载成功", "index");// 跳转首页页面
+ }
+
+
 }
